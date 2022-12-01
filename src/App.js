f84ced37e5496914c0f09da7332cdd750e350f8d
bryanparmelee <bryan.parmelee@gmail.com>
2022-11-30 23:23:16 -0500

    Add initial styling, background images

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import CONTEXT_DATA from "./data";
 
 function App() {
   const [quote, setQuote] = useState({});
+  const [bgIndex, setBgIndex] = useState(1);
   
   const getRandomQuote = () => {
     return CONTEXT_DATA[Math.floor(Math.random() * CONTEXT_DATA.length)];
@@ -12,14 +13,23 @@ function App() {
 
   useEffect(() => {
     setQuote(getRandomQuote);
+    setBgIndex(Math.floor(Math.random * 10));
   }, [])
 
-  const newQuote = () => setQuote(getRandomQuote);
+  const newQuote = () => {
+    setQuote(getRandomQuote);
+    setBgIndex(Math.floor(Math.random * 10));
+  }
   
   return (
-    <div className="App">
+    <div className={`h-screen w-screen p-8 bg-[url('./assets/galaxy4.jpg')] object-fill flex flex-col items-center justify-center`}>
       <Quote randomQuote={quote} />
-      <button onClick={newQuote}>Show Me Another</button>
+      <button 
+        className="border-2 p-4 rounded-xl"
+        onClick={newQuote}
+      >
+        Show Me Another
+      </button>
     </div>
   );
 }
--- src/Quote.jsx
+++ src/Quote.jsx
@@ -1,9 +1,11 @@
 const Quote = ({randomQuote}) => {
     const { quote, author } = randomQuote;
     return (
-        <div>
-            <h2>{quote}</h2>
-            <h5>{author}</h5>
+        <div className="text-center">
+            <h2 className="font-serif text-7xl font-bold text-white">
+                {`"${quote}"`}
+            </h2>
+            <h5 className="font-sans text-white text-lg pt-6 pr-12 text-right">{`— ${author}`}</h5>
         </div>
     )
 }
