38f7d576149fd7d87561ff5fff796d635d985a5b
bryanparmelee <bryan.parmelee@gmail.com>
2022-12-02 17:24:31 -0500

    Add quotes

--- src/data.js
+++ src/data.js
@@ -1,311 +1,311 @@
 const CONTEXT_DATA = [
   {
-    "QUOTE": "Rarrrrrr. I'm a giant pipe!",
-    "AUTHOR": "Jake Teufert"
+    "quote": "Rarrrrrr. I'm a giant pipe!",
+    "author": "Jake Teufert"
   },
   {
-    "QUOTE": "There's no fun in fundraising. It's just draising.",
-    "AUTHOR": "Jake Teufert"
+    "quote": "There's no fun in fundraising. It's just draising.",
+    "author": "Jake Teufert"
   },
   {
-    "QUOTE": "No, he's not a wizard.",
-    "AUTHOR": "Sammy Graham"
+    "quote": "No, he's not a wizard.",
+    "author": "Sammy Graham"
   },
   {
-    "QUOTE": "I wanna feel my pants vibrate.",
-    "AUTHOR": "Obdulio Ochoa"
+    "quote": "I wanna feel my pants vibrate.",
+    "author": "Obdulio Ochoa"
   },
   {
-    "QUOTE": "Sammy, you're like Jesus.",
-    "AUTHOR": "Jack(cheuk) Leung"
+    "quote": "Sammy, you're like Jesus.",
+    "author": "Jack(cheuk) Leung"
   },
   {
-    "QUOTE": "¿Dónde está la biblioteca?",
-    "AUTHOR": "Jake Teufert"
+    "quote": "¿Dónde está la biblioteca?",
+    "author": "Jake Teufert"
   },
   {
-    "QUOTE": "And that's why I eat crayons.",
-    "AUTHOR": "Jack(cheuk) Leung"
+    "quote": "And that's why I eat crayons.",
+    "author": "Jack(cheuk) Leung"
   },
   {
-    "QUOTE": "It's hard to un-give up.",
-    "AUTHOR": "Paul Shepard"
+    "quote": "It's hard to un-give up.",
+    "author": "Paul Shepard"
   },
   {
-    "QUOTE": "I'm a chicken stripper.",
-    "AUTHOR": "Myk Dunn"
+    "quote": "I'm a chicken stripper.",
+    "author": "Myk Dunn"
   },
   {
-    "QUOTE": "Lockheed Martin has a lot of trash.",
-    "AUTHOR": "Jack(cheuk) Leung"
+    "quote": "Lockheed Martin has a lot of trash.",
+    "author": "Jack(cheuk) Leung"
   },
   {
-    "QUOTE": "I've got plenty of stuff to break your knee caps with.",
-    "AUTHOR": "Jake Teufert"
+    "quote": "I've got plenty of stuff to break your knee caps with.",
+    "author": "Jake Teufert"
   },
   {
-    "QUOTE": "That move was juicier than a black belt sandwich.",
-    "AUTHOR": "Sammy Graham"
+    "quote": "That move was juicier than a black belt sandwich.",
+    "author": "Sammy Graham"
   },
   {
-    "QUOTE": "They'll catch some airtime and then they'll find out whether they chose the right God.",
-    "AUTHOR": "Jared Bowman"
+    "quote": "They'll catch some airtime and then they'll find out whether they chose the right God.",
+    "author": "Jared Bowman"
   },
   {
-    "QUOTE": "Feel that? That's what 2 inches gets you.",
-    "AUTHOR": "Jared Bowman"
+    "quote": "Feel that? That's what 2 inches gets you.",
+    "author": "Jared Bowman"
   },
   {
-    "QUOTE": "They sacrifice their head, like a snack.",
-    "AUTHOR": "Sammy Graham"
+    "quote": "They sacrifice their head, like a snack.",
+    "author": "Sammy Graham"
   },
   {
-    "QUOTE": "Give me sock, or give me stock!!",
-    "AUTHOR": "Jack(cheuk) Leung"
+    "quote": "Give me sock, or give me stock!!",
+    "author": "Jack(cheuk) Leung"
   },
   {
-    "QUOTE": "Thickness is the name of the game.",
-    "AUTHOR": "Jared Bowman"
+    "quote": "Thickness is the name of the game.",
+    "author": "Jared Bowman"
   },
   {
-    "QUOTE": "You'll look into my eyes and you'll just see excel in my retinas.",
-    "AUTHOR": "Jeremy Barton"
+    "quote": "You'll look into my eyes and you'll just see excel in my retinas.",
+    "author": "Jeremy Barton"
   },
   {
-    "QUOTE": "Then the drugs start working their way through my system and I realize there is no book in my hands at all.",
-    "AUTHOR": "Jared Bowman"
+    "quote": "Then the drugs start working their way through my system and I realize there is no book in my hands at all.",
+    "author": "Jared Bowman"
   },
   {
-    "QUOTE": "I bet a banana white claw would be interesting.",
-    "AUTHOR": "Brad Hoover"
+    "quote": "I bet a banana white claw would be interesting.",
+    "author": "Brad Hoover"
   },
   {
-    "QUOTE": "You just got played by a fish.",
-    "AUTHOR": "Sammy Graham"
+    "quote": "You just got played by a fish.",
+    "author": "Sammy Graham"
   },
   {
-    "QUOTE": "I like the pain... you know?",
-    "AUTHOR": "Jeremy Barton"
+    "quote": "I like the pain... you know?",
+    "author": "Jeremy Barton"
   },
   {
-    "QUOTE": "In Slack, no one can hear you scream.",
-    "AUTHOR": "Paul Shepard"
+    "quote": "In Slack, no one can hear you scream.",
+    "author": "Paul Shepard"
   },
   {
-    "QUOTE": "What was it at what and what was it at what?",
-    "AUTHOR": "Jeremy Barton"
+    "quote": "What was it at what and what was it at what?",
+    "author": "Jeremy Barton"
   },
   {
-    "QUOTE": "If it did work, someone would probably have done it.",
-    "AUTHOR": "Christian Hallstrom"
+    "quote": "If it did work, someone would probably have done it.",
+    "author": "Christian Hallstrom"
   },
   {
-    "QUOTE": "What if we double nub?",
-    "AUTHOR": "Steve Mauthe"
+    "quote": "What if we double nub?",
+    "author": "Steve Mauthe"
   },
   {
-    "QUOTE": "It's out of context for me, and I was there.",
-    "AUTHOR": "Mike Ryan"
+    "quote": "It's out of context for me, and I was there.",
+    "author": "Mike Ryan"
   },
   {
-    "QUOTE": "18-year single-malt sass.",
-    "AUTHOR": "Jake Teufert"
+    "quote": "18-year single-malt sass.",
+    "author": "Jake Teufert"
   },
   {
-    "QUOTE": "It's good when I can use human words.",
-    "AUTHOR": "Jared Bowman"
+    "quote": "It's good when I can use human words.",
+    "author": "Jared Bowman"
   },
   {
-    "QUOTE": "Don't take pictures of me when I'm in my down moment.",
-    "AUTHOR": "Jeremy Barton"
+    "quote": "Don't take pictures of me when I'm in my down moment.",
+    "author": "Jeremy Barton"
   },
   {
-    "QUOTE": "A triangle is just a sine wave on a diet.",
-    "AUTHOR": "Kyle Nguyen"
+    "quote": "A triangle is just a sine wave on a diet.",
+    "author": "Kyle Nguyen"
   },
   {
-    "QUOTE": "Compliment and commitment, two different things.",
-    "AUTHOR": "Jack(cheuk) Leung"
+    "quote": "Compliment and commitment, two different things.",
+    "author": "Jack(cheuk) Leung"
   },
   {
-    "QUOTE": "[O]ne of the steps is boiling the wood in H2O2.",
-    "AUTHOR": "Riley Wilbur"
+    "quote": "[O]ne of the steps is boiling the wood in H2O2.",
+    "author": "Riley Wilbur"
   },
   {
-    "QUOTE": "I'm going to start a rubber ducky cult.",
-    "AUTHOR": "Obdulio Ochoa"
+    "quote": "I'm going to start a rubber ducky cult.",
+    "author": "Obdulio Ochoa"
   },
   {
-    "QUOTE": "It does doing things once.",
-    "AUTHOR": "Jared Bowman"
+    "quote": "It does doing things once.",
+    "author": "Jared Bowman"
   },
   {
-    "QUOTE": "My nipple is rolling across the floor!",
-    "AUTHOR": "Mike Ryan"
+    "quote": "My nipple is rolling across the floor!",
+    "author": "Mike Ryan"
   },
   {
-    "QUOTE": "But who really knows anything? Not me.",
-    "AUTHOR": "Jeremy Barton"
+    "quote": "But who really knows anything? Not me.",
+    "author": "Jeremy Barton"
   },
   {
-    "QUOTE": "The best way to mitigate a risk is to become the risk.",
-    "AUTHOR": "Jack(cheuk) Leung"
+    "quote": "The best way to mitigate a risk is to become the risk.",
+    "author": "Jack(cheuk) Leung"
   },
   {
-    "QUOTE": "Jared is my ablative heat shield.",
-    "AUTHOR": "Jack(cheuk) Leung"
+    "quote": "Jared is my ablative heat shield.",
+    "author": "Jack(cheuk) Leung"
   },
   {
-    "QUOTE": "Speaking of getting stabbed in the back...",
-    "AUTHOR": "Riley Wilbur"
+    "quote": "Speaking of getting stabbed in the back...",
+    "author": "Riley Wilbur"
   },
   {
-    "QUOTE": "What other phrases would you like me to misquote you on?",
-    "AUTHOR": "Riley Wilbur"
+    "quote": "What other phrases would you like me to misquote you on?",
+    "author": "Riley Wilbur"
   },
   {
-    "QUOTE": "Arathane is the hammer of the adhesives world.",
-    "AUTHOR": "Riley Wilbur"
+    "quote": "Arathane is the hammer of the adhesives world.",
+    "author": "Riley Wilbur"
   },
   {
-    "QUOTE": "I should be a channel!",
-    "AUTHOR": "Sammy Graham"
+    "quote": "I should be a channel!",
+    "author": "Sammy Graham"
   },
   {
-    "QUOTE": "Jeff would be an otter.",
-    "AUTHOR": "Jake Teufert"
+    "quote": "Jeff would be an otter.",
+    "author": "Jake Teufert"
   },
   {
-    "QUOTE": "I'm just here for the pickaxe.",
-    "AUTHOR": "Gabe Mailhot"
+    "quote": "I'm just here for the pickaxe.",
+    "author": "Gabe Mailhot"
   },
   {
-    "QUOTE": "I don't like something you just said.",
-    "AUTHOR": "Jake Teufert"
+    "quote": "I don't like something you just said.",
+    "author": "Jake Teufert"
   },
   {
-    "QUOTE": "Sounds like me after eating Taco Bell.",
-    "AUTHOR": "Jeremy Barton"
+    "quote": "Sounds like me after eating Taco Bell.",
+    "author": "Jeremy Barton"
   },
   {
-    "QUOTE": "There's an oat in my throat.",
-    "AUTHOR": "Richard Stelling"
+    "quote": "There's an oat in my throat.",
+    "author": "Richard Stelling"
   },
   {
-    "QUOTE": "I'm more trustrworthy than Facebook and less trustworthy than Reddit.",
-    "AUTHOR": "Paul Shepard"
+    "quote": "I'm more trustrworthy than Facebook and less trustworthy than Reddit.",
+    "author": "Paul Shepard"
   },
   {
-    "QUOTE": "Dunkin Donuts is the Velveeta of of donuts.",
-    "AUTHOR": "Paul Shepard"
+    "quote": "Dunkin Donuts is the Velveeta of of donuts.",
+    "author": "Paul Shepard"
   },
   {
-    "QUOTE": "I'm a professional janker myself.",
-    "AUTHOR": "Jordy Fraties"
+    "quote": "I'm a professional janker myself.",
+    "author": "Jordy Fraties"
   },
   {
-    "QUOTE": "Will the real Jeremy Barton please stand up?",
-    "AUTHOR": "Obdulio Ochoa"
+    "quote": "Will the real Jeremy Barton please stand up?",
+    "author": "Obdulio Ochoa"
   },
   {
-    "QUOTE": "The more boring it is, the better it's going.",
-    "AUTHOR": "Sammy Graham"
+    "quote": "The more boring it is, the better it's going.",
+    "author": "Sammy Graham"
   },
   {
-    "QUOTE": "The intent is to use the that as the primary secondary.",
-    "AUTHOR": "Obdulio Ochoa"
+    "quote": "The intent is to use the that as the primary secondary.",
+    "author": "Obdulio Ochoa"
   },
   {
-    "QUOTE": "Jake: Killer of pixies.",
-    "AUTHOR": "Paul Shepard"
+    "quote": "Jake: Killer of pixies.",
+    "author": "Paul Shepard"
   },
   {
-    "QUOTE": "I'm full on taco beans.",
-    "AUTHOR": "Brad Hoover"
+    "quote": "I'm full on taco beans.",
+    "author": "Brad Hoover"
   },
   {
-    "QUOTE": "Take the apple out of the apple and put it right back in the apple.",
-    "AUTHOR": "Myk Dunn"
+    "quote": "Take the apple out of the apple and put it right back in the apple.",
+    "author": "Myk Dunn"
   },
   {
-    "QUOTE": "We can do Edward Thrusterhands.",
-    "AUTHOR": "Chris Carella"
+    "quote": "We can do Edward Thrusterhands.",
+    "author": "Chris Carella"
   },
   {
-    "QUOTE": "I'll draw cartoons anytime.",
-    "AUTHOR": "Jeff Gibson"
+    "quote": "I'll draw cartoons anytime.",
+    "author": "Jeff Gibson"
   },
   {
-    "QUOTE": "I was pretty let down when Jimbo told me I need 6mm -- or more.",
-    "AUTHOR": "Jack(cheuk) Leung"
+    "quote": "I was pretty let down when Jimbo told me I need 6mm -- or more.",
+    "author": "Jack(cheuk) Leung"
   },
   {
-    "QUOTE": "The test stand is not for testing test stands, it's not a test stand test stand.",
-    "AUTHOR": "Mike Ryan"
+    "quote": "The test stand is not for testing test stands, it's not a test stand test stand.",
+    "author": "Mike Ryan"
   },
   {
-    "QUOTE": "I'm going to find that old man and take his hat.",
-    "AUTHOR": "Mike Ryan"
+    "quote": "I'm going to find that old man and take his hat.",
+    "author": "Mike Ryan"
   },
   {
-    "QUOTE": "I take the work out of the work and then put it right back in the work.",
-    "AUTHOR": "Myk Dunn"
+    "quote": "I take the work out of the work and then put it right back in the work.",
+    "author": "Myk Dunn"
   },
   {
-    "QUOTE": "You're not important enough for me to be pissed.",
-    "AUTHOR": "Jake Teufert"
+    "quote": "You're not important enough for me to be pissed.",
+    "author": "Jake Teufert"
   },
   {
-    "QUOTE": "One second of butt-clenching dread.",
-    "AUTHOR": "Jake Teufert"
+    "quote": "One second of butt-clenching dread.",
+    "author": "Jake Teufert"
   },
   {
-    "QUOTE": "We need a real person sniffing paint and glue.",
-    "AUTHOR": "Jack(cheuk) Leung"
+    "quote": "We need a real person sniffing paint and glue.",
+    "author": "Jack(cheuk) Leung"
   },
   {
-    "QUOTE": "Day 2: I feel like there are slugs in my body from my vaseline.",
-    "AUTHOR": "Jeremy Barton"
+    "quote": "Day 2: I feel like there are slugs in my body from my vaseline.",
+    "author": "Jeremy Barton"
   },
   {
-    "QUOTE": "Tubes!",
-    "AUTHOR": "Jake Teufert"
+    "quote": "Tubes!",
+    "author": "Jake Teufert"
   },
   {
-    "QUOTE": "If you wanna sniff glue, that's the one to sniff.",
-    "AUTHOR": "Riley Wilbur"
+    "quote": "If you wanna sniff glue, that's the one to sniff.",
+    "author": "Riley Wilbur"
   },
   {
-    "QUOTE": "I need you, Brad.",
-    "AUTHOR": "Jack(cheuk) Leung"
+    "quote": "I need you, Brad.",
+    "author": "Jack(cheuk) Leung"
   },
   {
-    "QUOTE": "Spiritually, it's a flat spin.",
-    "AUTHOR": "Jake Teufert"
+    "quote": "Spiritually, it's a flat spin.",
+    "author": "Jake Teufert"
   },
   {
-    "QUOTE": "Did Walton steal the baby?",
-    "AUTHOR": "Myk Dunn"
+    "quote": "Did Walton steal the baby?",
+    "author": "Myk Dunn"
   },
   {
-    "QUOTE": "Give me the little man.",
-    "AUTHOR": "Myk Dunn"
+    "quote": "Give me the little man.",
+    "author": "Myk Dunn"
   },
   {
-    "QUOTE": "Salt water is just a sad plasma.",
-    "AUTHOR": "Jake Teufert"
+    "quote": "Salt water is just a sad plasma.",
+    "author": "Jake Teufert"
   },
   {
-    "QUOTE": "What the nptF?",
-    "AUTHOR": "Riley Wilbur"
+    "quote": "What the nptF?",
+    "author": "Riley Wilbur"
   },
   {
-    "QUOTE": "I was weirded out by the polynomial.",
-    "AUTHOR": "Richard Stelling"
+    "quote": "I was weirded out by the polynomial.",
+    "author": "Richard Stelling"
   },
   {
-    "QUOTE": "I was raised by a forest.",
-    "AUTHOR": "Jeff Gibson"
+    "quote": "I was raised by a forest.",
+    "author": "Jeff Gibson"
   }
 ]
 
