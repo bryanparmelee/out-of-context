8bbab25c38dd38b241a2164585971bc60596cd53
bryanparmelee <bryan.parmelee@gmail.com>
2022-12-14 19:08:09 -0500

    Add text animations

--- src/Quote.jsx
+++ src/Quote.jsx
@@ -2,10 +2,12 @@ const Quote = ({randomQuote}) => {
     const { quote, author } = randomQuote;
     return (
         <div className="text-center">
-            <h2 className="font-serif text-3xl sm:text-6xl font-bold text-white text-shadow-lg">
+            <h2 key={quote} 
+                className="animate-quote-fade font-serif text-3xl sm:text-6xl font-bold text-white text-shadow-lg">
                 {`"${quote}"`}
             </h2>
-            <h5 className="font-sans text-white text-md sm:text-lg pt-6 pr-12 text-right text-shadow-lg">
+            <h5 key={author}
+                className="animate-author-fade font-sans text-white text-md sm:text-lg pt-6 pr-12 text-right text-shadow-lg">
                 {`— ${author}`}
             </h5>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,40 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'fade-in': {
+          '0%': { 
+            opacity: '0',
+            transform: 'translateY(-5px)'
+           },
+          '100%': { 
+            opacity: '1',
+            transform: 'translateY(0)'
+          },
+        },
+        'fade-left': {
+          '0%, 75%': {
+            opacity: '0',
+            transform: 'translateX(10px)'
+          },
+  
+          '100%': {
+            opacity: '1',
+            transform: 'translateX(0)'
+          },
+        }
+      }, 
+      animation: {
+        'quote-fade': 'fade-in 1s ease-in',
+        'author-fade': 'fade-left 1.2s ease-in'
+      }
+    },
     fontFamily: {
       'sans': ['Roboto', 'sans-serif'],
       'serif': ['EB Garamond', 'serif']
     }
+
   },
   plugins: [],
 }
